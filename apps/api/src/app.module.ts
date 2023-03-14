a82bcc7991e5e7aa6905d6a8aa2700d1bc003ad3
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-03-14 22:23:12 +0530

    add sample gql resource

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ItemModule } from './item/item.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+    }),
+    ItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: ['https://studio.apollographql.com', 'http://localhost:3000'],
+    methods: 'POST',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  })
   await app.listen(3000)
 }
 bootstrap()
